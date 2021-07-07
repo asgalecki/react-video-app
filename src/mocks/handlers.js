4c86c27ef6dbb591566b4027af19c9502fe3e684
ag-vectivus <a.s.galecki@gmail.com>
2021-07-07 18:00:05 +0200

    Update test mocks handlers.js

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,3 +1,24 @@
 import { rest } from "msw";
 
-export const handlers = [];
+export const handlers = [
+	rest.get("https://www.googleapis.com/youtube/v3/videos", (req, res, ctx) => {
+		return res(
+			ctx.status(200),
+			ctx.json({
+				items: [
+					{
+						id: "p5kcBxL7-qI",
+						type: "youtube",
+						title: "Star Trek: The Next Generation theme (HQ)",
+						likes: "11431",
+						views: "1719938",
+						thumbnail: "https://i.ytimg.com/vi/p5kcBxL7-qI/mqdefault.jpg",
+						embed: "https://www.youtube.com/embed/p5kcBxL7-qI",
+						added: Date.now(),
+						isFavourite: false,
+					},
+				],
+			})
+		);
+	}),
+];
